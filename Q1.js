63e9832e94a2557f69af8df496e183352175bda4
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-10-31 17:26:28 +0530

    updates

---
+++ Q1.js
@@ -0,0 +1,26 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+//even no
+// for(let i = 0; i < 10; i++) {
+//     if(arr[i]%2 == 0) {
+//         console.log("even no", arr[i])
+//     }
+// }
+
+
+// natural no
+// for(let i = 0; i <= 10; i++){
+//     console.log('natural no', i)
+// }
+
+
+//odd no
+// for(let i = 0; i < arr.length; i++) {
+//     if(arr[i]%2 == 1) {
+//         console.log('odd no', arr[i])
+//     }
+// }
+
+
+//
